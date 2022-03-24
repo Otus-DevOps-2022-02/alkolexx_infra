67bb444646b36a475333dcd1278a44ea708c94d1
ALEKSEY LOKTIONOV <alkolexx@gmail.com>
2022-03-25 01:25:53 +0300

    CLOUD-TESTAPP-FIX-MONGODB

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,3 @@ sudo apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
-ps aux | grep puma
--- install_ruby.sh
+++ install_ruby.sh
@@ -2,5 +2,3 @@
 
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
-ruby -v
-bundler -v
