6d754ce5f521d3d976073adbcd777c122ebc1758
Aleksey Loktionov <alkolexx@gmail.com>
2022-04-12 15:55:56 +0300

    HW-07

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
@@ -5,16 +14,21 @@ provider "yandex" {
   zone                     = var.zone
 }
 
+module "vpc" {
+  source = "../modules/vpc"
+  zone   = var.zone
+}
+
 module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
+  subnet_id       = module.vpc.subnet_id
 }
 
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
-  subnet_id       = var.subnet_id
+  subnet_id       = module.vpc.subnet_id
 }
